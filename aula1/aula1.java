5fda5254daaba7c4299e6283e49c7e3ebc852089
Isabelli-constante <isabelliconstante1605@hotmail.com>
2023-04-14 14:05:18 -0300

    summary

---
+++ aula1/aula1.java
@@ -0,0 +1,16 @@
+// -> É um comentário de 1 linha  
+
+/*
+ Esse é o nosso 1° programa em Java
+ Ele fará a impressão de uma mensagem na tela
+ */
+public class aula1{
+    
+//Método main: Inicia a nossa aplicação Java
+public static void main(String args[]){
+    System.out.println("desistindo estou");
+    System.out.println("quero minha casa");
+    System.out.println("vou sumir");
+
+}
+}
